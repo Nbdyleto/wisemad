e1caeeaaffd04cf3783e21f07056ea737e8ea272
Nbdyleto <nobodyleto2004@gmail.com>
2023-07-17 15:08:33 -0300

    Initial commit
    setup vue.js and tailwindcss

---
+++ wise_frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="font-extrabold text-2xl">Wisemad is perfect!</h1>
+  </main>
+</template>
